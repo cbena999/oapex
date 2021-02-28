0b1df057f76df58c3ac300cf26bcb232a85f045e
carlosbyahoo <cbena999@yahoo.com.mx>
2021-02-28 07:59:14 -0600

    borrados las copias _.sql

--- apex-learn1/schemas/ordsdemo/INDEXES/SYS_C0012638_1.sql
+++
@@ -1,7 +0,0 @@
---------------------------------------------------------
---  DDL for Index SYS_C0012638
---------------------------------------------------------
-
-  CREATE UNIQUE INDEX "ORDSDEMO"."SYS_C0012638" ON "ORDSDEMO"."TestCollection111" ("ID") 
-  PCTFREE 10 INITRANS 2 MAXTRANS 255 COMPUTE STATISTICS 
-  TABLESPACE "USERS" ;
--- apex-learn1/schemas/ordsdemo/INDEXES/SYS_C0012747_1.sql
+++
@@ -1,7 +0,0 @@
---------------------------------------------------------
---  DDL for Index SYS_C0012747
---------------------------------------------------------
-
-  CREATE UNIQUE INDEX "ORDSDEMO"."SYS_C0012747" ON "ORDSDEMO"."TestCollection1111" ("ID") 
-  PCTFREE 10 INITRANS 2 MAXTRANS 255 COMPUTE STATISTICS 
-  TABLESPACE "USERS" ;
