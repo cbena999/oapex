20198f210105c35eabafc40028e097fe43b589c2
carlosbyahoo <cbena999@yahoo.com.mx>
2021-03-06 10:32:18 -0600

    se añadio funcion y type para split multiples values get ws

--- apex-learn1/schemas/ordsdemo/PACKAGE_BODIES/MY_GLOBALS.sql
+++ apex-learn1/schemas/ordsdemo/PACKAGE_BODIES/MY_GLOBALS.sql
@@ -166,6 +166,23 @@ RETURN TRUE;
 
 END;
 
+
+FUNCTION in_list (p_in_list  IN  VARCHAR2)
+  RETURN t_in_list_tab PIPELINED
+AS
+  l_text  VARCHAR2(32767) := p_in_list || ',';
+  l_idx   NUMBER;
+BEGIN
+  LOOP
+    l_idx := INSTR(l_text, ',');
+    EXIT WHEN NVL(l_idx, 0) = 0;
+    PIPE ROW (TRIM(SUBSTR(l_text, 1, l_idx - 1)));
+    l_text := SUBSTR(l_text, l_idx + 1);
+  END LOOP;
+
+  RETURN;
+END;
+
 END;
 /
 
